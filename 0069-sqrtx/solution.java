fc8f72bbf148db8f09968a3bd7ac3ca0a219031a
Kartikey Saxena <saxenakartikey208@gmail.com>
2024-03-10 10:42:03 +0000

    Sync LeetCode submission Runtime - 1 ms (97.88%), Memory - 40.7 MB (99.99%)

--- 0069-sqrtx/solution.java
+++ 0069-sqrtx/solution.java
@@ -1,22 +1,17 @@
-class Solution
-{
-    public int mySqrt(int x) 
-    {
-        if(x==0 || x==1)
-        return x;
-
-        int min=1;
-        int max=x;
-        int mid;
-        while(min<=max){
-            mid=min + (max-min)/2;
-            if((long)mid*mid > (long)x)
-             max=mid-1;
-            else if(mid*mid==x)
-             return mid;
-            else
-             min = mid+1;  
+class Solution {
+    public int mySqrt(int x) {
+        int end,start;
+        start=1;
+        end=x;
+        while(start<=end)
+        {
+            long mid=start+(end-start)/2;
+            long z=mid*mid;
+            if(z==x || (z<x && (mid+1)*(mid+1)>x))
+             return (int)mid;
+            else if(z>x) end=(int)mid-1;
+            else start=(int)mid+1;
         }
-        return Math.round(max);
+        return 0;
     }
 }
