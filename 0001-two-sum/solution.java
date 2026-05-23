e48286604436590dfdf8203033b5b0e2dca0cac2
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-05-23 18:02:30 +0000

    Sync LeetCode submission Runtime - 2 ms (99.39%), Memory - 47.2 MB (39.53%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,23 +1,13 @@
-class Solution
-{
-    public int[] twoSum(int[] nums, int target) 
-    {
-        int size=nums.length;
-        int a[]=new int[2];
-         int i,j,sum;
-         for(i=0;i<size;i++)
-         {   
-            for(j=i+1;j<size;j++)
-            {
-                sum=nums[i]+nums[j];
-                if(sum==target)
-                {
-                    a[0]=i;
-                    a[1]=j;
-                }
-                
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int c=target-nums[i];
+            if(map.containsKey(c)){
+                return new int[] {map.get(c),i};
             }
-         }
-     return a; }
-
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
 }
