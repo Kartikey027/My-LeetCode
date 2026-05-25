88d63542a224b050439f9378eba53884729ca4c3
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-05-25 14:40:08 +0000

    Sync LeetCode submission Runtime - 3 ms (87.55%), Memory - 43.1 MB (72.30%)

---
+++ 0020-valid-parentheses/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char x=st.pop();
+                if(ch=='}'&&x!='{'){
+                    return false;
+                }
+                if(ch==')'&& x!='('){
+                    return false;
+                }
+                if(ch==']'&&x!='['){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
