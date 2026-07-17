307acd9690f2adf95fe68008431e029914a70636
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-07-17 18:13:18 +0530

    Time: 1 ms (94.22%), Space: 43.6 MB (15.82%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(numRows==0) return ans;
+        for(int i=0;i<numRows;i++){
+            List<Integer> l=new ArrayList<>();
+            for(int j=0;j<i+1;j++){
+                if(j==0 || j==i){
+                    l.add(1);
+                }
+                else{
+                    l.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+                }
+            }
+            ans.add(l);
+        }
+        return ans;
+        
+    }
+}
