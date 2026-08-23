2523fb29380619cd7b82bc0272bda3b3491a11c6
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-08-24 01:45:31 +0530

    Time: 5 ms (24.65%), Space: 50 MB (85.53%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,8 +1,14 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int dp[][]=new int[grid.length][grid[0].length];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
+        int m,n;
+        m=grid.length;
+        n=grid[0].length;
+        int dp[][]=new int[m][n];
+        for(int i[]:dp){
+            Arrays.fill(i,-1);
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(i==0 && j==0){
                     dp[i][j]=grid[i][j];
                 }
@@ -13,10 +19,10 @@ class Solution {
                     dp[i][j]=dp[i-1][j]+grid[i][j];
                 }
                 else{
-                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+                    dp[i][j]=Math.min(dp[i][j-1]+grid[i][j],dp[i-1][j]+grid[i][j]);
                 }
             }
         }
-        return dp[grid.length-1][grid[0].length-1];
+        return dp[m-1][n-1];
     }
 }
