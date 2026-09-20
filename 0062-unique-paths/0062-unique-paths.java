4b8bf1da18ff1361fe77b7ae2d3bf857d31cb25b
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-09-20 19:12:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -8,10 +8,10 @@ class Solution {
         return solve(m,n,0,0);
     }
     public int solve(int m,int n,int i,int j){
-        if(i>=m || j>=n){
+        if(i==m && j==n){
             return 0;
         }
-        if(i==m-1 && j==n-1){
+        if(i==m-1 || j==n-1){
             return 1;
         }
         if(dp[i][j]!=-1){
@@ -19,5 +19,6 @@ class Solution {
         }
         dp[i][j]=solve(m,n,i+1,j)+solve(m,n,i,j+1);
         return dp[i][j];
+        
     }
 }
