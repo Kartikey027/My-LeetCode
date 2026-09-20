38038b1e7e60626439280e822c52f29f8451d124
Kartikey Saxena <saxenakartikey208@gmail.com>
2026-09-20 23:16:18 +0530

    Time: 6 ms (58.74%), Space: 47.6 MB (8.97%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int dp[][]=new int[matrix.length][matrix.length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length;j++){
-                if(i==0){
-                    dp[i][j]=matrix[i][j];
+        int n = matrix.length;
+        int[] dp = new int[n];
+
+        for (int j = 0; j < n; j++) {
+            dp[j] = matrix[n - 1][j];
+        }
+        for (int i=n - 2;i>=0;i--) {
+
+            int[] curr =new int[n];
+            for (int j=0;j<n;j++) {
+                int min =dp[j];
+                if (j > 0) {
+                    min = Math.min(min, dp[j - 1]);
                 }
-                else if(j==0){
-                    dp[i][j]=Math.min(dp[i-1][j],dp[i-1][j+1])+matrix[i][j];
+                if (j<n-1) {
+                    min = Math.min(min, dp[j + 1]);
                 }
-                else if(j==matrix.length-1){
-                    dp[i][j]=Math.min(dp[i-1][j-1],dp[i-1][j])+matrix[i][j];
-                }
-                else{
-                    dp[i][j]=Math.min(dp[i-1][j+1],Math.min(dp[i-1][j-1],dp[i-1][j]))+matrix[i][j];
-                }
+                curr[j] = matrix[i][j] + min;
             }
+            dp = curr;
         }
-        int min=Integer.MAX_VALUE;
-        for(int i:dp[dp.length-1]){
-            if(i<min) min=i;
+        int ans=Integer.MAX_VALUE;
+        for (int x : dp) {
+            ans = Math.min(ans, x);
         }
-        return min;
+        return ans;
     }
 }
